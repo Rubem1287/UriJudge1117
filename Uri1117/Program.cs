d949e0d8843a699370ee412b1ff0d6486c42387d
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:18:48 -0300

    Add files via upload

---
+++ Uri1117/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+namespace Uri1117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            double a, b, media;
+
+
+            
+
+
+           
+               
+                a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                while (a < 0 || a > 10)
+                {
+                    Console.WriteLine("Nota inválida");
+
+                    a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                }
+
+                b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                while (b < 0 || b > 10)
+                {
+                    Console.WriteLine("Nota inválida");
+
+                    b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                }
+
+                media = (a + b) / 2.0;
+
+                Console.WriteLine("Média = " + media.ToString("F2", CultureInfo.InvariantCulture));
+              
+            }
+        }
+    }
+
